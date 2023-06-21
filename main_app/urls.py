2360c3f9afd4cb62bd9daed3016beac518fda7fe
EricFithian <ef2413@columbia.edu>
2023-06-20 21:40:56 -0400

    Updated with models and create route plus admin console

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # This first path tells me that it's looking in views.py for a class that's named Home at localhost:8000/
     path('', views.Home.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),
     path('artists/', views.ArtistList.as_view(), name="artist_list"),
-    path('songs/', views.SongList.as_view(), name="song_list")
+    path('songs/', views.SongList.as_view(), name="song_list"),
+    # This is telling me if I hit the URL artists/new, I should see a class named ArtistCreate in the views
+    path('artists/new/', views.ArtistCreate.as_view(), name="artist_create")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Artist
+# This will import the class we are extending 
+from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
 
 # Create your views here.
+# I created a class named Home that's a child of TemplateView and is inheriting what's built into the parent class
 class Home(TemplateView):
     template_name = "home.html"
-    
+
 class About(TemplateView):
     template_name = "about.html"
 
@@ -16,12 +20,6 @@ class SongList(TemplateView):
         context["songs"] = songs
         return context
 
-class Artist:
-    def __init__(self, name, image, bio):
-        self.name = name
-        self.image = image
-        self.bio = bio
-
 class Song:
     def __init__(self, title, album):
         self.title = title
@@ -32,25 +30,24 @@ songs = [
     Song("Never Ending Song", "Never Ending Song")
 ]
 
-artists = [
-  Artist("Gorillaz", "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29",
-          "Gorillaz are once again disrupting the paradigm and breaking convention in their round the back door fashion with Song Machine, the newest concept from one of the most inventive bands around."),
-  Artist("Panic! At The Disco",
-          "https://i.scdn.co/image/58518a04cdd1f20a24cf0545838f3a7b775f8080", "Welcome 👋 The Amazing Beebo was working on a new bio but now he's too busy taking over the world."),
-  Artist("Joji", "https://i.scdn.co/image/7bc3bb57c6977b18d8afe7d02adaeed4c31810df",
-          "Joji is one of the most enthralling artists of the digital age. New album Nectar arrives as an eagerly anticipated follow-up to Joji's RIAA Gold-certified first full-length album BALLADS 1, which topped the Billboard R&B / Hip-Hop Charts and has amassed 3.6B+ streams to date."),
-  Artist("Metallica",
-          "https://i1.sndcdn.com/artworks-001099370752-qhj3j0-t500x500.jpg", "Metallica formed in 1981 by drummer Lars Ulrich and guitarist and vocalist James Hetfield and has become one of the most influential and commercially successful rock bands in history, having sold 110 million albums worldwide while playing to millions of fans on literally all seven continents."),
-  Artist("Bad Bunny",
-          "https://m.media-amazon.com/images/I/91pz0YqKG9L._AC_SX466_.jpg", "Benito Antonio Martínez Ocasio, known by his stage name Bad Bunny, is a Puerto Rican rapper, singer, and songwriter. His music is often defined as Latin trap and reggaeton, but he has incorporated various other genres into his music, including rock, bachata, and soul"),
-  Artist("Kaskade",
-          "https://i1.sndcdn.com/artworks-sNjd3toBZYCG-0-t500x500.jpg", "Ryan Gary Raddon, better known by his stage name Kaskade, is an American DJ, record producer, and remixer."),
-]
+class ArtistCreate(CreateView):
+    model = Artist
+    fields = ['name', 'img', 'bio', 'verified_artist']
+    template_name = "artist_create.html"
+    success_url = '/artists/'
 
 class ArtistList(TemplateView):
     template_name = "artist_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["artists"] = artists
+        # This is essentially a query built into Django that's saying SELECT * FROM main_app_artist
+        # self.request.GET is a dictionary just like in express we have req.query
+        # print(self.request.GET)
+        name = self.request.GET.get('name')
+        if name != None:
+            # This is essentially a regex matcher that's built in for us. It's searching for anything where the name of the artist contains (at any point) this name if there is a query
+            context["artists"] = Artist.objects.filter(name__icontains=name)
+        else:
+            context["artists"] = Artist.objects.all()
         return context
